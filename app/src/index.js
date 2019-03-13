0e6fb8984ee14bb1f3d3428134f014bb15d4aeca
yehia67 <yehiatarek67@gmail.com>
2019-03-13 21:46:38 +0200

    add line charts for results

--- app/src/index.js
+++ app/src/index.js
@@ -84,6 +84,53 @@ const App = {
         result = await seeResult(_sessionName).call();
         alert(result);
     },
+    drawChart: function() {
+
+        var chBar = document.getElementById("chBar");
+        var chartData = {
+            labels: ["Bad", "Normal", "Good", "Very Good", "Excellent"],
+            datasets: [{
+                    data: [589, 445, 483, 503, 689, 692, 634],
+                    backgroundColor: colors[0]
+                },
+                {
+                    data: [209, 245, 383, 403, 589, 692, 580],
+                    backgroundColor: colors[1]
+                },
+                {
+                    data: [489, 135, 483, 290, 189, 603, 600],
+                    backgroundColor: colors[2]
+                },
+                {
+                    data: [639, 465, 493, 478, 589, 632, 674],
+                    backgroundColor: colors[4]
+                }
+            ]
+        };
+
+        if (chBar) {
+            new Chart(chBar, {
+                type: 'bar',
+                data: chartData,
+                options: {
+                    scales: {
+                        xAxes: [{
+                            barPercentage: 0.9,
+                            categoryPercentage: 0.5
+                        }],
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+        }
+    }
 
 };
 
