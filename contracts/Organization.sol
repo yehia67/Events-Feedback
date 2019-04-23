5ca4780599314c11c4dce3842ca5ac56359b72fc
yehia67 <yehiatarek67@gmail.com>
2019-04-23 23:46:55 +0200

    fix bugs

--- contracts/Organization.sol
+++ contracts/Organization.sol
@@ -3,23 +3,23 @@ pragma solidity >=0.4.22 <0.6.0;
 contract Organization {
 
      address creator;
-     event createdSession(string name, address creator);
+     event sessionnCreated(string name,address sessionAddress ,address creator);
      
       modifier onlyCreator(){
         require(msg.sender == creator);
         _;
     }
      function createdSession(
-      string _sessionName,
-      string _description,
+      string memory _sessionName,
+      string memory _description,
       uint _startTime,
       uint _endTime,
-      address[] _lecturer,
-      address[] _attendes;
+      address[] memory _lecturer,
+      address[] memory _attendes
      ) public  onlyCreator returns(address) {
         
         Session sessionAddress = new Session(_sessionName , _description , _startTime , _endTime, _lecturer,_attendes );
-        emit sessionnCreated(_name, institutionAddress, creator);
+        emit sessionnCreated(_sessionName,sessionAddress,creator);
         return sessionAddress;
      }
   
@@ -31,27 +31,27 @@ contract Session {
      
       address public creator;  // The address of parent Organization
      
-      string sessionName,
-      string description,
-      uint startTime,
-      uint endTime,
-      address[] lecturer,
+      string sessionName;
+      string description;
+      uint startTime;
+      uint endTime;
+      address[] lecturer;
       address[] attendes; 
 
       uint8[] result; 
       mapping(address => uint) public attendes_feedback; //institution boardMembers
   
-      constructor (string _sessionName, string _description, uint _startTime,uint _endTime,address _lecturer,address _attendes) public{
+      constructor (string memory _sessionName, string memory _description, uint _startTime,uint _endTime,address[] memory   _lecturer,address[] memory  _attendes) public{
               sessionName =  _sessionName;
               description = _description;
               startTime = _startTime;
               endTime = _endTime;
-              attendes = __attendes;
+              attendes = _attendes;
               lecturer = _lecturer;
               initAttendes(attendes);
       } 
 
-    function initAttendes(address[] _attendes) private{
+    function initAttendes(address[] memory _attendes) private{
            for(uint i=0 ; i < _attendes.length ; i++){
             attendes_feedback[_attendes[i]] = -1;
         }
